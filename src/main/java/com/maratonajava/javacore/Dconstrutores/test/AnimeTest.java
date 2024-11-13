bb256f5c9ce8fed14787a86645a088e1318d9c4c
Fabiano Cardoso <sl.biano28@gmail.com>
2024-11-13 17:31:18 -0300

    feat: aula58 construtores

---
+++ src/main/java/com/maratonajava/javacore/Dconstrutores/test/AnimeTest.java
@@ -0,0 +1,12 @@
+package com.maratonajava.javacore.Dconstrutores.test;
+
+import com.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Akuma Drive 2", "TV", 1, "Ação");
+        anime.imprime();
+        Anime anime1 = new Anime();
+        anime1.imprime();
+    }
+}
