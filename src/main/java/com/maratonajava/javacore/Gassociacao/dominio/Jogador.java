4ce29540a559b58a593e19a23fe31e4e8a9ea3f7
Fabiano Cardoso <sl.biano28@gmail.com>
2024-11-20 20:02:45 -0300

    feat: aula65 Associação um para muitos

--- src/main/java/com/maratonajava/javacore/Gassociacao/dominio/Jogador.java
+++ src/main/java/com/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -2,9 +2,21 @@ package com.maratonajava.javacore.Gassociacao.dominio;
 
 public class Jogador {
     private String nome;
+    private Time time;
 
     public void imprime() {
         System.out.println("Nome: " + this.nome);
+        if (this.time != null) {
+            System.out.println("Time: " + time.getNome());
+        }
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 
     public Jogador(String nome) {
---
+++ src/main/java/com/maratonajava/javacore/Gassociacao/test/JogadorTest02.java
@@ -0,0 +1,14 @@
+package com.maratonajava.javacore.Gassociacao.test;
+
+import com.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import com.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Time time = new Time("Seleção Brasileira");
+
+        jogador1.setTime(time);
+        jogador1.imprime();
+    }
+}
